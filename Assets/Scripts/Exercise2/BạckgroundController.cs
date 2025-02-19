baa6bca5128d9c5f18d3005bb87ee0d45c2413f6
NGUYEN HOANG LONG <145125551+Starlightll@users.noreply.github.com>
2025-02-19 20:17:05 +0700

    .

---
+++ Assets/Scripts/Exercise2/BạckgroundController.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BạckgroundController : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    [SerializeField] private Renderer backgroundRenderer;
+
+    private List<GameObject> backgrounds = new List<GameObject>();
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        backgroundRenderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0) ;
+    }
+}
